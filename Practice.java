197c5c7f25927cf4f742aaa663a82cbea3039a65
uuz495 <g3295694687@gmail.com>
2025-05-28 16:15:29 +0800

    get up!

--- Practice.java
+++ Practice.java
@@ -1,34 +1,57 @@
-import java.util.ArrayList;
-public class Practice {
-   public static void main(String[]args) {
-      ArrayList<Student> uuz = new ArrayList<>();
-      Student s1 = new Student("Cell", 11, "male");
-      Student s2 = new Student("Sky" , 18 , "femboy");
-      Student s3 = new Student("reium" , 15 , "female");
-      uuz.add(s1);
-      uuz.add(s2);
-      uuz.add(s3);
-      System.out.println(Tool.getMaxage(uuz));
+class Animal {
+   int age;
+   String color;
+   public Animal() {
+   }
+   public Animal(int age, String color) {
+      this.age = age;
+      this.color = color;
+   }
+   void eat(String something) {
+
    }
 }
-class Student {
+class Cat extends Animal {
+   void catchMouse() {
+      System.out.println("the "+color+" cat is catching mouse. ");
+   };
+   void eat(String something) {
+      System.out.println("the cat is eating fish. ");
+   }
+   public Cat() {
+      super();
+   }
+   public Cat(int age, String color) {
+      super(age, color);
+   }
+}
+class Dog extends Animal {
+   void lookHome() {
+      System.out.println("the "+color+" dog is guarding home. ");
+   }
+   void eat(String somrthing) {
+      System.out.println("the dog is licking bone");
+   }
+   public Dog() {
+      super();
+   }
+   public Dog(int age, String color) {
+      super(age, color);
+   }
+}
+class Person {
    String name;
    int age;
-   String gender;
-   public Student(String name, int age, String gender) {
-      this.name = name;
-      this.age = age;
-      this.gender = gender;
-   }
-   public Student() {
+   void keepPet(Animal pet, String something){
+      pet.eat(something);
    }
 }
-class Tool {
-   public static int getMaxage(ArrayList<Student> uuz) {
-      int maxAge=uuz.get(0).age;
-      for(Student s : uuz) {
-         if(s.age > maxAge) maxAge = s.age;
-      }
-      return maxAge;
+public class Practice {
+   public static void main(String[]args) {
+      Person p = new Person();
+      Animal dog = new Dog(19, "angry");
+      Animal cat = new Cat(18, "calm");
+      p.keepPet(dog, null);
+      p.keepPet(cat, null);
    }
 }
