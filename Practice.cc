d385713682dcfd355ad1214fbac657c4f408a6f0
uuz495 <g3295694687@gmail.com>
2025-06-18 15:58:15 +0800

    revise some mistake

--- Practice.cc
+++ Practice.cc
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello Yuyuko" << endl;
+    int x , y;
+    x = 5 & 6;
+    y = 5 | 6;
+    cout<< x << endl << y;
+    int z = 8;
+    cout<< ~z;
     return 0;
 }
